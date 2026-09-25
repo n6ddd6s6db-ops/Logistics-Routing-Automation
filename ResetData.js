c80392e32be00b578c44d4971f96acf9fbbbe534
Aleš Botezatu <n6ddd6s6db@privaterelay.appleid.com>
2026-09-25 23:38:29 +0200

    Update ResetData.js

--- ResetData.js
+++ ResetData.js
@@ -2,6 +2,7 @@
  * ============================================================================
  * CLEAN SLATE - Clears Input, Output, and resets Planning sheet.
  * ============================================================================
+ *@author Aleš Botezatu
  */
 function clearAllData() {
   console.log("⚙️ Routing engine designed and developed by Aleš Botezatu.");
